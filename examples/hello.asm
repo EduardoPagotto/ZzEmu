9fa5ebdd75581f57013e7381a231c022a9c4430c
Eduardo Pagotto <edupagotto@gmail.com>
2021-04-13 12:50:12 -0300

    melhoria no asm

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,24 +1,70 @@
+;===========================================================
 ; Primeiro Teste
-org 0x0000
+;
+; Autor : Eduardo Pagotto
+; Data: 2020-04-13 
+;
+;===========================================================
 
 ram:        equ 0100h   ; Inicio da ram, cfg em Console.TotROM
 top:        equ 0200h   ; Fim da ram, cfg em Console.TotROM + Connsole.SizeRAM
+dev1:       equ 0x01
+dev2:       equ 0x02
 
-start:  LD SP, top      ; fixa o SP no fima da memoria ram
-        LD A, 0x10
-        LD (ram), A
-        LD A, 0x0
-        LD A,(ram)
-        CP 0x10
-        JP NZ,fim
-        DI
-        ; rst     0x0
-fim:    halt
-; Data
-; ram_top:        defw 0x0030
-ds              10, 0xee
-defb            0xff,0xff,0xff
-;line1:    defb ' primeira,',13,'$'
-;line2_3:  defb ' segunda,',13,'terceita,',13,'$'
-;line4:    defb ' quarta.',13,13,'$'
-end
+org 0x0000
+        di
+        jp boot
+org 0008h
+        jp bool_frio
+org 0038h
+        push af
+        push bc
+        push de
+        push hl
+        push ix
+        push iy
+        exx
+        call exec_interrupt
+        exx
+        pop iy
+        pop ix
+        pop hl
+        pop de
+        pop bc
+        pop af
+        reti
+exec_interrupt:
+        nop
+        ret 
+boot:
+        ld sp, top
+        in a,(dev1)
+        set 0x01, a
+        ld (ram), a
+        set 0x04, a
+        ld (ram+1), a
+        out (dev2), a
+        ei
+        halt
+bool_frio:
+        halt
+
+
+; start:  LD SP, top      ; fixa o SP no fima da memoria ram
+;         LD A, 0x10
+;         LD (ram), A
+;         LD A, 0x0
+;         LD A,(ram)
+;         CP 0x10
+;         JP NZ,fim
+;         DI
+;         ; rst     0x0
+; fim:    halt
+; ; Data
+; ; ram_top:        defw 0x0030
+; ds              10, 0xee
+; defb            0xff,0xff,0xff
+; ;line1:    defb ' primeira,',13,'$'
+; ;line2_3:  defb ' segunda,',13,'terceita,',13,'$'
+; ;line4:    defb ' quarta.',13,13,'$'
+end.
